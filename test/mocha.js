d895bf20bfd559f5d993fae2e2b97a23daee8e84
DogJun <498343874@qq.com>
2017-11-15 21:43:24 +0800

    feat: 添加持续集成

---
+++ test/mocha.js
@@ -0,0 +1,15 @@
+const {should, expect, assert} = require('chai')
+const {add, mul, cover} = require('../src/math')
+
+describe('#math', () => {
+  describe('add', () => {
+    it('should return 5 when 2 + 3', () => {
+      expect(add(2, 3), 5)
+    })
+  })
+  describe('mul', () => {
+    it('should return 6 when 2 * 3', () => {
+      expect(mul(2, 3), 6)
+    })
+  })
+})
